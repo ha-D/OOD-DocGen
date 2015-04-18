d86b8cdd5dccca02124b2411f6e071cd363ba424
ha-D <hadi.zolfaghaari@gmail.com>
2015-04-18 23:35:18 +0430

    Create project structure

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/script.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/script.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed',
+          compass: true
+        },
+        files: {
+          'dist/styles.min.css': 'src/scss/main.scss'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['src/**/*.js'],
+        tasks: ['jshint', 'uglify']  
+      },
+      css: {
+        files: 'src/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('test', ['jshint']);
+
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'sass']);
+
+};
