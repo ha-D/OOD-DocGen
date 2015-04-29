2fb88901faf32cc76118cf5249a8553bd6debcdc
ha-D <hadi.zolfaghaari@gmail.com>
2015-04-29 21:51:27 +0430

    Implement glossary page

--- dist/script.js
+++ dist/script.js
@@ -6,7 +6,8 @@ var app = angular.module('myApp', [
 
 app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
-	.when('/', { templateUrl: 'partials/usecases.html' });
+	.when('/usecase', { templateUrl: 'partials/usecases.html' })
+    .when('/glossary', { templateUrl: 'partials/glossary.html' });
 }]);
 
 app.config( [
@@ -201,7 +202,6 @@ function UseCasePageCtrlImpl ($scope, $http, $timeout, $mdDialog, store) {
 		$http.get('partials/usecase.tex').success(function (source) {
 			var template = Handlebars.compile(source);
 			var subsystems = $scope.renderSubsystems();
-			console.log(subsystems);
 			var content = template({subsystems: subsystems});
 
 			$scope.texContent = content;
@@ -216,11 +216,11 @@ function UseCasePageCtrlImpl ($scope, $http, $timeout, $mdDialog, store) {
 	};
 
 	$scope.save = function () {
-		store.save({key: 'subsystems', subsystems: $scope.data.subsystems, snippets: $scope.data.snippets});
+		store.save({key: 'usecase', subsystems: $scope.data.subsystems, snippets: $scope.data.snippets});
 	};
 
 	$scope.load = function() {
-		store.get('subsystems', function (data) {
+		store.get('usecase', function (data) {
 			if (data) {
 				$scope.data.subsystems = data.subsystems;
 				$scope.data.snippets = data.snippets;
@@ -237,8 +237,7 @@ function UseCasePageCtrlImpl ($scope, $http, $timeout, $mdDialog, store) {
 }
 
 function UseCasePageCtrl ($scope, $http, $timeout, $mdDialog) {
-	$scope.testlist = ["hasdsad", "asdad", 'asd'];
-	var store =  new Lawnchair({name:'usecase'}, function(store) {
+	var store =  new Lawnchair({name:'docgen'}, function(store) {
 		UseCasePageCtrlImpl($scope, $http, $timeout, $mdDialog, this);
 	});
 }
@@ -319,7 +318,154 @@ function UseCaseEditJSONCtrl ($scope, $mdDialog, $mdToast, data) {
 
 app.controller('UseCasePageCtrl',['$scope', '$http', '$timeout', '$mdDialog', UseCasePageCtrl]);
 app.controller('UseCaseDetailsCtrl',['$scope', '$mdDialog', 'usecase', UseCaseDetailsCtrl]);
-app.controller('UseCaseEditJSONCtrl',['$scope', '$mdDialog', '$mdToast', 'data', UseCaseEditJSONCtrl]);;var usecaseIndex = 1;
+app.controller('UseCaseEditJSONCtrl',['$scope', '$mdDialog', '$mdToast', 'data', UseCaseEditJSONCtrl]);;function GlossaryPageCtrlImpl ($scope, $http, $timeout, $mdToast, store) {
+	var templateWord = {
+		word: "",
+		definition: "",
+		synonyms: [],
+		antonyms: []
+	};
+	
+	$scope.state = '';
+
+	$scope.manual = {
+		content: ''
+	};
+
+	$scope.tex = {
+		link: '',
+		content: ''
+	};
+
+	$scope.words = [];
+	$scope.wordForm = angular.copy(templateWord);
+
+	$scope.clearForm = function () {
+		$scope.wordForm = angular.copy(templateWord);
+		$scope.save();
+	};
+
+	$scope.addWord = function () {
+		if ($scope.wordForm.word) {
+			$scope.words.push($scope.wordForm);
+			$scope.clearForm();
+			$scope.save();
+		}	
+	};
+
+	$scope.removeWord = function (index) {
+		$scope.words.splice(index, 1);
+		$scope.save();
+	};
+
+	$scope.editWord = function (word) {
+		$scope.wordForm = word;
+	};
+
+	$scope.manualEditStart = function () {
+		$scope.state = 'manual';
+		$scope.manual.content = angular.toJson($scope.words, 4);
+	};
+
+	$scope.manualEditSave = function () {
+		try {
+			$scope.words = angular.fromJson($scope.manual.content);
+			$scope.save();
+		} catch(e) {
+			$mdToast.show($mdToast.simple().content('Invalid JSON'));
+		}
+		$scope.state = '';
+	};
+
+	$scope.manualEditCancel = function () {
+		$scope.state = '';
+	};
+
+	$scope.renderTex = function () {
+		function categorize () {
+			var cats = {};
+
+			for (var i = 0; i < $scope.words.length; i++) {
+				var word = $scope.words[i];
+				if (!cats[word.word[0]]) {
+					cats[word.word[0]] = [];
+				}
+				cats[word.word[0]].push(word);
+			}
+
+			if (cats['آ']) {
+				if (cats['ا']) {
+					cats['ا'].push(cats['آ']);
+				} else {
+					cats['ا'] = cats['آ'];
+				}
+				delete cats['آ'];
+			}
+
+			var catList = [];
+			for (var key in cats) {
+				catList.push({
+					letter: key,
+					words: cats[key]
+				});
+			}
+
+			catList.sort(function (c1, c2) {
+				return c1.letter > c2.letter ? 1 : c1.letter < c2.letter ? -1 : 0;
+			});
+
+			return catList;
+		}
+
+		$http.get('partials/glossary.tex').success(function (source) {
+			var template = Handlebars.compile(source);
+			var words = $scope.words;
+			var content = template({wordCategories: categorize()});
+
+			$scope.tex.content = content;
+			$scope.tex.link =  encodeURIComponent(content);
+			$scope.state = 'tex';
+		});
+	};
+
+	$scope.texCancel = function () {
+		$scope.tex.link = '';
+		$scope.tex.content = '';
+		$scope.state = '';
+	};
+
+	$scope.sort = function () {
+		$scope.words.sort(function (w1, w2) {
+			return w1.word > w2.word ? 1 : w1.word < w2.word ? -1 : 0;
+		});
+	};
+
+	$scope.save = function () {
+		$scope.sort();
+		store.save({key: 'glossary', words: $scope.words});
+	};
+
+	$scope.load = function() {
+		store.get('glossary', function (data) {
+			if (data) {
+				$scope.words = data.words;
+			} else {
+				$scope.words = [];
+			}
+		});
+	};
+
+	$scope.load();
+}
+
+function GlossaryPageCtrl ($scope, $http, $timeout, $mdToast) {
+	var store =  new Lawnchair({name:'docgen'}, function(store) {
+		GlossaryPageCtrlImpl($scope, $http, $timeout, $mdToast, this);
+	});
+}
+
+app.controller('GlossaryPageCtrl',['$scope', '$http', '$timeout', '$mdToast', GlossaryPageCtrl]);
+;var usecaseIndex = 1;
 
 Handlebars.registerHelper('reset', function() {
   	usecaseIndex = 1;
---
+++ src/js/glossary.js
@@ -0,0 +1,147 @@
+function GlossaryPageCtrlImpl ($scope, $http, $timeout, $mdToast, store) {
+	var templateWord = {
+		word: "",
+		definition: "",
+		synonyms: [],
+		antonyms: []
+	};
+	
+	$scope.state = '';
+
+	$scope.manual = {
+		content: ''
+	};
+
+	$scope.tex = {
+		link: '',
+		content: ''
+	};
+
+	$scope.words = [];
+	$scope.wordForm = angular.copy(templateWord);
+
+	$scope.clearForm = function () {
+		$scope.wordForm = angular.copy(templateWord);
+		$scope.save();
+	};
+
+	$scope.addWord = function () {
+		if ($scope.wordForm.word) {
+			$scope.words.push($scope.wordForm);
+			$scope.clearForm();
+			$scope.save();
+		}	
+	};
+
+	$scope.removeWord = function (index) {
+		$scope.words.splice(index, 1);
+		$scope.save();
+	};
+
+	$scope.editWord = function (word) {
+		$scope.wordForm = word;
+	};
+
+	$scope.manualEditStart = function () {
+		$scope.state = 'manual';
+		$scope.manual.content = angular.toJson($scope.words, 4);
+	};
+
+	$scope.manualEditSave = function () {
+		try {
+			$scope.words = angular.fromJson($scope.manual.content);
+			$scope.save();
+		} catch(e) {
+			$mdToast.show($mdToast.simple().content('Invalid JSON'));
+		}
+		$scope.state = '';
+	};
+
+	$scope.manualEditCancel = function () {
+		$scope.state = '';
+	};
+
+	$scope.renderTex = function () {
+		function categorize () {
+			var cats = {};
+
+			for (var i = 0; i < $scope.words.length; i++) {
+				var word = $scope.words[i];
+				if (!cats[word.word[0]]) {
+					cats[word.word[0]] = [];
+				}
+				cats[word.word[0]].push(word);
+			}
+
+			if (cats['آ']) {
+				if (cats['ا']) {
+					cats['ا'].push(cats['آ']);
+				} else {
+					cats['ا'] = cats['آ'];
+				}
+				delete cats['آ'];
+			}
+
+			var catList = [];
+			for (var key in cats) {
+				catList.push({
+					letter: key,
+					words: cats[key]
+				});
+			}
+
+			catList.sort(function (c1, c2) {
+				return c1.letter > c2.letter ? 1 : c1.letter < c2.letter ? -1 : 0;
+			});
+
+			return catList;
+		}
+
+		$http.get('partials/glossary.tex').success(function (source) {
+			var template = Handlebars.compile(source);
+			var words = $scope.words;
+			var content = template({wordCategories: categorize()});
+
+			$scope.tex.content = content;
+			$scope.tex.link =  encodeURIComponent(content);
+			$scope.state = 'tex';
+		});
+	};
+
+	$scope.texCancel = function () {
+		$scope.tex.link = '';
+		$scope.tex.content = '';
+		$scope.state = '';
+	};
+
+	$scope.sort = function () {
+		$scope.words.sort(function (w1, w2) {
+			return w1.word > w2.word ? 1 : w1.word < w2.word ? -1 : 0;
+		});
+	};
+
+	$scope.save = function () {
+		$scope.sort();
+		store.save({key: 'glossary', words: $scope.words});
+	};
+
+	$scope.load = function() {
+		store.get('glossary', function (data) {
+			if (data) {
+				$scope.words = data.words;
+			} else {
+				$scope.words = [];
+			}
+		});
+	};
+
+	$scope.load();
+}
+
+function GlossaryPageCtrl ($scope, $http, $timeout, $mdToast) {
+	var store =  new Lawnchair({name:'docgen'}, function(store) {
+		GlossaryPageCtrlImpl($scope, $http, $timeout, $mdToast, this);
+	});
+}
+
+app.controller('GlossaryPageCtrl',['$scope', '$http', '$timeout', '$mdToast', GlossaryPageCtrl]);
