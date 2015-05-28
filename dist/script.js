308235bf3c12bf704f19b74604a7e0dea5dbf082
ha-D <hadi.zolfaghaari@gmail.com>
2015-05-28 15:01:35 +0430

    Add CRC pages

--- dist/script.js
+++ dist/script.js
@@ -7,7 +7,8 @@ var app = angular.module('myApp', [
 app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 	.when('/usecase', { templateUrl: 'partials/usecases.html' })
-    .when('/glossary', { templateUrl: 'partials/glossary.html' });
+    .when('/glossary', { templateUrl: 'partials/glossary.html' })
+    .when('/crc', { templateUrl: 'partials/crc.html' });
 }]);
 
 app.config( [
@@ -318,7 +319,155 @@ function UseCaseEditJSONCtrl ($scope, $mdDialog, $mdToast, data) {
 
 app.controller('UseCasePageCtrl',['$scope', '$http', '$timeout', '$mdDialog', UseCasePageCtrl]);
 app.controller('UseCaseDetailsCtrl',['$scope', '$mdDialog', 'usecase', UseCaseDetailsCtrl]);
-app.controller('UseCaseEditJSONCtrl',['$scope', '$mdDialog', '$mdToast', 'data', UseCaseEditJSONCtrl]);;function GlossaryPageCtrlImpl ($scope, $http, $timeout, $mdToast, store) {
+app.controller('UseCaseEditJSONCtrl',['$scope', '$mdDialog', '$mdToast', 'data', UseCaseEditJSONCtrl]);;function CRCPageCtrlImpl ($scope, $http, $timeout, $mdToast, store) {
+	var templateWord = {
+		className: "",
+		description: "",
+		superClass: "",
+		responsibilities: [],
+		collaborators: []
+	};
+	
+	$scope.state = '';
+
+	$scope.manual = {
+		content: ''
+	};
+
+	$scope.tex = {
+		link: '',
+		content: ''
+	};
+
+	$scope.words = [];
+	$scope.wordForm = angular.copy(templateWord);
+
+	$scope.clearForm = function () {
+		$scope.wordForm = angular.copy(templateWord);
+		$scope.save();
+	};
+
+	$scope.addWord = function () {
+		if ($scope.wordForm.className) {
+			$scope.words.push($scope.wordForm);
+			$scope.clearForm();
+			$scope.save();
+		}	
+	};
+
+	$scope.removeWord = function (index) {
+		$scope.words.splice(index, 1);
+		$scope.save();
+	};
+
+	$scope.editWord = function (word) {
+		$scope.wordForm = word;
+	};
+
+	$scope.manualEditStart = function () {
+		$scope.state = 'edit';
+		$scope.manual.content = angular.toJson($scope.words, 4);
+	};
+
+	$scope.manualEditSave = function () {
+		try {
+			$scope.words = angular.fromJson($scope.manual.content);
+			$scope.save();
+		} catch(e) {
+			$mdToast.show($mdToast.simple().content('Invalid JSON'));
+		}
+		$scope.state = '';
+	};
+
+	$scope.manualEditCancel = function () {
+		$scope.state = '';
+	};
+
+	$scope.renderTex = function () {
+		function categorize () {
+			var cats = {};
+
+			for (var i = 0; i < $scope.words.length; i++) {
+				var word = $scope.words[i];
+				if (!cats[word.word[0]]) {
+					cats[word.word[0]] = [];
+				}
+				cats[word.word[0]].push(word);
+			}
+
+			if (cats['آ']) {
+				if (cats['ا']) {
+					cats['ا'].push(cats['آ']);
+				} else {
+					cats['ا'] = cats['آ'];
+				}
+				delete cats['آ'];
+			}
+
+			var catList = [];
+			for (var key in cats) {
+				catList.push({
+					letter: key,
+					words: cats[key]
+				});
+			}
+
+			catList.sort(function (c1, c2) {
+				return c1.letter > c2.letter ? 1 : c1.letter < c2.letter ? -1 : 0;
+			});
+
+			return catList;
+		}
+
+		$http.get('partials/glossary.tex').success(function (source) {
+			var template = Handlebars.compile(source);
+			var words = $scope.words;
+			var content = template({wordCategories: categorize()});
+
+			$scope.tex.content = content;
+			$scope.tex.link =  encodeURIComponent(content);
+			$scope.state = 'tex';
+		});
+	};
+
+	$scope.texCancel = function () {
+		$scope.tex.link = '';
+		$scope.tex.content = '';
+		$scope.state = '';
+	};
+
+	$scope.sort = function () {
+		$scope.words.sort(function (w1, w2) {
+			return w1.word > w2.word ? 1 : w1.word < w2.word ? -1 : 0;
+		});
+	};
+
+	$scope.save = function () {
+		$scope.sort();
+		store.save({key: 'crc', words: $scope.words});
+	};
+
+	$scope.load = function() {
+		store.get('crc', function (data) {
+			if (data) {
+				$scope.words = data.words;
+			} else {
+				$scope.words = [];
+			}
+		});
+	};
+
+	$scope.load();
+}
+
+function CRCPageCtrl ($scope, $http, $timeout, $mdToast) {
+	var store =  new Lawnchair({name:'docgen'}, function(store) {
+		CRCPageCtrlImpl($scope, $http, $timeout, $mdToast, this);
+	});
+}
+
+app.controller('CRCPageCtrl',['$scope', '$http', '$timeout', '$mdToast', CRCPageCtrl]);
+;function GlossaryPageCtrlImpl ($scope, $http, $timeout, $mdToast, store) {
 	var templateWord = {
 		word: "",
 		definition: "",
